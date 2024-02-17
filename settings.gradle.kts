fc63c98d68b1a6da7c43a292b2d587290a47ba9d
eastperson <kjuioqq8@gmail.com>
2024-02-17 19:17:45 +0900

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "spring-tcc-pattern-example"
---
+++ src/main/kotlin/com/ep/springtccpatternexample/SpringTccPatternExampleApplication.kt
@@ -0,0 +1,11 @@
+package com.ep.springtccpatternexample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringTccPatternExampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringTccPatternExampleApplication>(*args)
+}
